3a1d13b78d50eda470da0814e30df6bedaf80afe
DavideDamico <davidedamico89@gmail.com>
2024-06-17 19:17:36 +0200

    Creazione e sistemazione pagine inglese , index porte portoni e infissi , creazione login e register , creazione di vari folder

---
+++ pages/js-files/registrazione.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+const homeLink = document.querySelector(".home-link");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+  homeLink.classList.remove("move-right");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.add("active");
+  homeLink.classList.add("move-right");
+});
